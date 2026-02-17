c77e73d55f128ba4db2db963e2b75fd42579cc86
M-Sanjay-IND <sanjayatinternet@gmail.com>
2026-02-17 07:26:32 +0530

    OOPSBannerPrint UC2

--- OOPSBanner.java
+++ OOPSBanner.java
@@ -1,5 +1,13 @@
-public class OOPSBanner {
+class OOPSBanner {
     public static void main(String[] args) {
-        System.out.println("OOPS");
+        System.out.println(" ********  " + " " + "  *******  " + " " + "  *******  " + " " + " *******");
+        System.out.println("**       **" + " " + "**       **" + " " + "**       **" + " " + "**       ");
+        System.out.println("**       **" + " " + "**       **" + " " + "**       **" + " " + "**       ");
+        System.out.println("**       **" + " " + "**       **" + " " + "**       **" + " " + "**       ");
+        System.out.println("**       **" + " " + "**       **" + " " + "*********  " + " " + " *******");
+        System.out.println("**       **" + " " + "**       **" + " " + "**         " + " " + "      **");
+        System.out.println("**       **" + " " + "**       **" + " " + "**         " + " " + "      **");
+        System.out.println("  *******   " + " " + " *******  " + " " + "**         " + " " + "*******");
+
     }
 }
